6332b2e12a9e2f4a0e4ef6e38add7976161c138c
Sean Widmier <seanwidmier@gmail.com>
2019-01-14 17:26:07 -0600

    create .gitignore, add DE-10 GHRD

--- led.v
+++
@@ -1,8 +0,0 @@
-module led(
-	LEDG
-);
-
-output [7:0] LEDG;
-
-assign LEDG[1] = 1'b1;
-endmodule
--- software/nios_1/obj/default/main.d
+++
@@ -1,3 +0,0 @@
-obj/default/main.o: main.c ../nios_1_bsp//HAL/inc/alt_types.h
-
-../nios_1_bsp//HAL/inc/alt_types.h:
